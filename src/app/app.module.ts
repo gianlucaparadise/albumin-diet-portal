0289099d2036b82d72d036e020204fe7d3b7fa40
gianlucaparadise <gianlucaparadise@gmail.com>
2018-12-16 17:56:47 +0100

    Initial setup created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TagListComponent } from './tag-list/tag-list.component';
+import { AlbumListComponent } from './album-list/album-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagListComponent,
+    AlbumListComponent
   ],
   imports: [
     BrowserModule,
