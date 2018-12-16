c99bb6380166a60aebb54c441ff37685739206cc
gianlucaparadise <gianlucaparadise@gmail.com>
2018-12-16 18:26:28 +0100

    chips list inserted as tag list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatChipsModule } from '@angular/material/chips';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagListComponent } from './tag-list/tag-list.component';
@@ -14,7 +17,9 @@ import { AlbumListComponent } from './album-list/album-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
