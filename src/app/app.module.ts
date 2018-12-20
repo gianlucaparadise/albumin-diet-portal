c6a1fbe5d6a568bd2d8a61e69eebf6859b68dd2f
gianlucaparadise <gianlucaparadise@gmail.com>
2018-12-20 06:16:37 +0100

    Play button on album

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatChipsModule, MatSidenavModule, MatCardModule } from '@angular/material';
+import { MatChipsModule, MatSidenavModule, MatCardModule, MatIconModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +21,7 @@ import { AlbumListComponent } from './album-list/album-list.component';
     MatSidenavModule,
     MatChipsModule,
     MatCardModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
