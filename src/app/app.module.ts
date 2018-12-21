1f21b267d65dd4c6d850bf99ce97c23158ddac73
gianlucaparadise <gianlucaparadise@gmail.com>
2018-12-21 07:43:21 +0100

    Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TagListComponent } from './tag-list/tag-list.component';
 import { AlbumListComponent } from './album-list/album-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TagListComponent,
-    AlbumListComponent
+    AlbumListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
