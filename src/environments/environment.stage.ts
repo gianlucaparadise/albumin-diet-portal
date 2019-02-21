df9aeeeabf3057f59146c7e2f25706539dfcd757
gianlucaparadise <gianlucaparadise@gmail.com>
2019-02-21 07:08:18 +0100

    Migrating to Heroku

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   name: 'stage',
   engineBaseUrl: 'https://albumin-diet-engine.herokuapp.com',
-  baseUrl: 'https://gianlucaparadise.github.io/albumin-diet-portal'
+  baseUrl: 'https://albumindiet.herokuapp.com'
 };
