794710a7ea9db0d1fca45fd74fca13346663f91c
gianlucaparadise <gianlucaparadise@gmail.com>
2019-06-03 06:06:13 +0200

    Scroll position restoration (TO BE IMPROVED)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router, NavigationStart, NavigationEnd, Event } from '@angular/router';
+import { filter, scan, observeOn } from 'rxjs/operators';
+import { asyncScheduler } from 'rxjs';
+
+interface ScrollPositionRestore {
+  event: Event;
+  positions: { [K: number]: number };
+  trigger: 'imperative' | 'popstate' | 'hashchange';
+  idToRestore: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild('contentArea', { read: ElementRef }) private contentArea: ElementRef<HTMLMainElement>;
+
+  title = 'albumin-diet-portal';
+
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private router: Router
   ) {
     this.matIconRegistry.addSvgIcon(
       `eggs_filled`,
@@ -22,5 +38,50 @@ export class AppComponent {
     );
   }
 
-  title = 'albumin-diet-portal';
+  ngOnInit() {
+    //#region Scroll position restoration
+    // Source: https://blog.angularindepth.com/reactive-scroll-position-restoration-with-rxjs-792577f842c
+    // FIXME: this is not working correctly.
+    this.router.events
+      .pipe(
+        filter(
+          event =>
+            event instanceof NavigationStart || event instanceof NavigationEnd,
+        ),
+        scan<Event, ScrollPositionRestore>((acc, event) => ({
+          event,
+          positions: {
+            ...acc.positions,
+            ...(event instanceof NavigationStart
+              ? {
+                  [event.id]: this.contentArea.nativeElement.scrollTop,
+                }
+              : {}),
+          },
+          trigger:
+            event instanceof NavigationStart
+              ? event.navigationTrigger
+              : acc.trigger,
+          idToRestore:
+            (event instanceof NavigationStart &&
+              event.restoredState &&
+              event.restoredState.navigationId + 1) ||
+            acc.idToRestore,
+        })),
+        filter(
+          ({ event, trigger }) => event instanceof NavigationEnd && !!trigger,
+        ),
+        observeOn(asyncScheduler),
+      )
+      .subscribe(({ trigger, positions, idToRestore }) => {
+        if (trigger === 'imperative') {
+          this.contentArea.nativeElement.scrollTop = 0;
+        } else if (trigger === 'popstate') {
+          this.contentArea.nativeElement.scrollTop = positions[idToRestore];
+        } else {
+          console.log(`trigger not handled: ${trigger}`);
+        }
+      });
+    //#endregion
+  }
 }
